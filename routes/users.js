8f4fbdb903956a48dc6c41ef735af0ef43a94f55
aman_d1996 <aman_d1996@yahoo.com>
2016-07-21 20:29:00 +0530

    latest

--- routes/users.js
+++ routes/users.js
@@ -2,9 +2,6 @@ var express = require('express');
 var usersController = express.Router();
 
 /* Chatroom */
-usersController.get('/', function(req, res) {
-    res.send("Thanks for coming. To join the chatroom, insert your name after the chat/ in the url and press Enter. :)");
-});
 
 usersController.get('/:user_name', function(req, res) {
     res.render('users', { user_name: req.params.user_name });
