290fd449c094b235a33b251b01551d71a23ea334
aman_d1996 <aman_d1996@yahoo.com>
2016-07-21 18:44:59 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+#Chatroom Server
+
+##Chatroom Server based on Nodejs.
+##Made on ExpressJS platform
+##Run as: node app
---
+++ routes/users.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var usersController = express.Router();
+
+/* Chatroom */
+usersController.get('/', function(req, res) {
+    res.send("Thanks for coming. To join the chatroom, insert your name after the chat/ in the url and press Enter. :)");
+});
+
+usersController.get('/:user_name', function(req, res) {
+    res.render('users', { user_name: req.params.user_name });
+});
+
+module.exports = usersController;
